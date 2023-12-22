893a72c8513def05c3222b8b298e8921e403fc35
cash-i1 <cash@DESKTOP-C2JKRK6>
2023-12-22 15:47:02 +1100

    Added sounds 😐 and more shit code.

--- main.cpp
+++ main.cpp
@@ -38,7 +38,8 @@ bool isOnGround(Player p, bool check_collision = false, Rectangle col = Rectangl
                 platform_rect.width = platforms[platform].size.x;
                 platform_rect.height = platforms[platform].size.y;
 
-                if (CheckCollisionRecs(col, platform_rect)) {
+                if (CheckCollisionRecs(col, platform_rect) && col.y+1 < platforms[platform].position.y) {
+                    cout << col.y << ", " << platforms[platform].position.y << endl;
                     return true;
                 }
             }
@@ -50,13 +51,13 @@ bool isOnGround(Player p, bool check_collision = false, Rectangle col = Rectangl
 }
 
 
-float platformYOffset = 0;
+float platformYOffset = 0.0;
 
 void genPlatforms() {
     Platform newPlatform = Platform();
 
     newPlatform.color = RED;
-    newPlatform.position.y = -platformYOffset + 100;
+    newPlatform.position.y = -platformYOffset + 1000;
     newPlatform.position.x = GetRandomValue(0, 400);
     newPlatform.size.x = 80;
     newPlatform.size.y = 20;
@@ -69,20 +70,31 @@ void genPlatforms() {
 void drawPlatforms() {
     for (int platform = 0; platform < platforms.size(); platform++) {
         DrawRectangle(platforms[platform].position.x, platforms[platform].position.y, platforms[platform].size.x, platforms[platform].size.y, RED);
+
+        // if (GetRandomValue(0,10)) {
+        //     DrawRectangle(platforms[platform].position.x, platforms[platform].position.y - 30, 20, 20, GREEN);
+        //     vector<Platform>::iterator it = platforms.begin() + 2;
+        //     platforms.erase(it);
+        // }
     }
 }
 
 
 int main() {
     InitWindow(WIDTH, HEIGHT, "Doodle UP!");
-
+    InitAudioDevice();
     
+    Sound sound = LoadSound("assets/sincity.mp3");
+    PlaySound(sound);
+
+    Sound fart = LoadSound("assets/fart.mp3");
+
     Player p = Player();
 
     p.texture = LoadTexture("assets/player_1.png");
     p.velocity.y = 1;
     p.speed = Vector2 {2.0, 2.0};
-    p.jumpForce = 1500;
+    p.jumpForce = 420;
     p.textureScale = 6;
     Rectangle playerCollision;
 
@@ -90,6 +102,8 @@ int main() {
     const char* debug_info;
 
     Camera2D cam;
+
+    int score;
 
     while (!WindowShouldClose()) {
         float delta = GetFrameTime();
@@ -101,6 +115,7 @@ int main() {
             
         } else {
             if (IsKeyDown(KEY_SPACE)) {
+                PlaySound(fart);
                 p.velocity.y = -p.jumpForce;
             } else {
                 p.velocity.y = 0;
@@ -127,15 +142,26 @@ int main() {
         test.y = 700;
 
         cam.zoom = 1.0f;
-        cam.target.y = p.position.y - (HEIGHT/2);
-        cam.target.x = p.position.x - WIDTH/2 + (p.texture.width*p.textureScale) / 2;
+        if (p.position.y < HEIGHT/2) {
+            cam.target.y = p.position.y - (GetScreenHeight()/2);
+        } else {
+            // cam.target.y += 1;
+        }
+        
+        cam.target.x = p.position.x - GetScreenWidth()/2 + (p.texture.width*p.textureScale) / 2;
         cam.rotation = 0.0f;
 
         genPlatforms();
 
-        cout << p.position.x << ", " << p.position.y << "; " << cam.target.x << ", " << cam.target.y << endl;
+        // cout << p.position.x << ", " << p.position.y << "; " << cam.target.x << ", " << cam.target.y << endl;
         
+        if ((int)p.position.y % 100 == 0) {
+            score += 1;
+        }
+
         BeginDrawing();
+
+        
             BeginMode2D(cam);
             ClearBackground(BLUE);
 
@@ -143,9 +169,13 @@ int main() {
             DrawRectangleRec(playerCollision, GREEN);
             drawPlatforms();
 
-            DrawText(debug_info, 10, 34, 40, GREEN);
+            
             
             EndMode2D();
+
+            // DrawText(debug_info, 10, 34, 40, GREEN);
+            
+            DrawText(TextFormat("Score: %d", score), 10, 10, 20, GREEN);
         EndDrawing();
         
     }
